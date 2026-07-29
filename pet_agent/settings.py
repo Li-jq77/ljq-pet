271488e7720b1e737678ae8d53d0c8fa0ec46bcd
Li-jq77 <1554832220@qq.com>
2026-07-29 16:49:15 +0800

    pymysql置顶

--- pet_agent/settings.py
+++ pet_agent/settings.py
@@ -9,11 +9,10 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
-from pathlib import Path
-import os
 import pymysql
 pymysql.install_as_MySQLdb()
+from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
